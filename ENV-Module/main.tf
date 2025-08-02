a4c60deab4dd795a94022bfea3401c59aa8a9399
cpg4590 <chandrapal31@outlook.com>
2025-08-02 19:20:17 +0530

    modified

--- ENV-Module/main.tf
+++ ENV-Module/main.tf
@@ -3,23 +3,23 @@ module "resource_group" {
   eah-rg = var.eah-rg
 }
 
-module "vnet" {
-  source     = "../Infra_Module/EAH-Vnet"
-  eah-vnet   = var.eah-vnet
-  depends_on = [module.resource_group]
-}
+# module "vnet" {
+#   source     = "../Infra_Module/EAH-Vnet"
+#   eah-vnet   = var.eah-vnet
+#   depends_on = [module.resource_group]
+# }
 
-module "subnet" {
-  source     = "../Infra_Module/EAH-Subnet"
-  eah-subnet = var.eah-subnet
-  depends_on = [module.vnet]
-}
+# module "subnet" {
+#   source     = "../Infra_Module/EAH-Subnet"
+#   eah-subnet = var.eah-subnet
+#   depends_on = [module.vnet]
+# }
 
 
-module "linux_vm" {
-  source       = "../Infra_Module/EAH-Linux_VM"
-  eah-linux-vm = var.eah-linux-vm
-  eah-nic      = var.eah-nic
-  eah-subnet   = var.eah-subnet
-  depends_on   = [module.subnet, module.resource_group, module.vnet]
-}
+# module "linux_vm" {
+#   source       = "../Infra_Module/EAH-Linux_VM"
+#   eah-linux-vm = var.eah-linux-vm
+#   eah-nic      = var.eah-nic
+#   eah-subnet   = var.eah-subnet
+#   depends_on   = [module.subnet, module.resource_group, module.vnet]
+# }
